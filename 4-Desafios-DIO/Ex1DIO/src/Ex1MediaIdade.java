7175c40219214c0cc8ea8fabffb92ba49d227c29
Caroline <caroline.anbs@gmail.com>
2022-11-29 15:26:11 -0300

    Primeiro Desafio Bootcamp QA ok

--- 4-Desafios-DIO/Ex1DIO/src/Ex1MediaIdade.java
+++ 4-Desafios-DIO/Ex1DIO/src/Ex1MediaIdade.java
@@ -2,24 +2,27 @@ import java.util.Scanner;
 
 public class Ex1MediaIdade {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int idade1;
-        int idade2;
-        int idade3;
-        int media;
-
-        System.out.println("Idade: ");
-        idade1 = scan.nextInt();
-        idade2 = scan.nextInt();
-        idade3 = scan.nextInt();
-
-        if (media >= 0 && media <= 25){
-            System.out.println("Esta turma é Jovem");
-            } else if (media >= 26 && media <= 60){
-            System.out.println("Esta turma é Adulta");
-            }else{
-               System.out.println("Esta turma é Idosa");
-            }
+        Scanner sc = new Scanner(System.in);
+		
+		int idade1, idade2, idade3, media;
+		
+		
+		System.out.println("Digite suas idades:");
+		System.out.print("Idade 1: "); idade1 = sc.nextInt(); 
+		System.out.print("Idade 2: "); idade2 = sc.nextInt();
+		System.out.print("Idade 3: "); idade3 = sc.nextInt();
+		System.out.println();
+		
+		media = (idade1 + idade2 + idade3)/3;
+		
+		if(media >= 0 && media <=25) {
+			System.out.println("Jovem!");
+		}else if(media >= 26 && media <= 60) {
+			System.out.println("Adulta!");
+		}else {
+			System.out.println("Idosa!");
+		}
+		
+		sc.close();
 }
 }
