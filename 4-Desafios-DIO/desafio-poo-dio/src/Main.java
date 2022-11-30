36943421fa997106859dcbfe92f581a8aecc5160
Caroline <caroline.anbs@gmail.com>
2022-11-30 12:16:51 -0300

    Abstração e encapsulamento

---
+++ 4-Desafios-DIO/desafio-poo-dio/src/Main.java
@@ -0,0 +1,33 @@
+import diodesafio.dominio.Curso;
+import diodesafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do Curso de Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("Descrição do Curso de Javascript");
+        curso2.setCargaHoraria(13);
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição da Mentoria sobre Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+
+    }
+    
+}
